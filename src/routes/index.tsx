420540129221b9905f4c4018d757651d381a0add
rpgraul <rpgraul@hotmail.com>
2026-08-13 16:42:06 -0300

    feat: remove EmailTest page and related email sending functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,11 @@ import ProtectedRoute from '@/components/auth/ProtectedRoute'
 import UserManagement from '@/components/admin/UserManagement'
 import DashboardPage from '@/routes/DashboardPage'
 import TaskPage from '@/routes/TaskPage'
-import EmailTest from '@/pages/EmailTest'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/test-email" element={<EmailTest />} />
       <Route path="/dashboard" element={<ProtectedRoute />}>
         <Route index element={<DashboardPage />} />
         <Route path="task/:taskId" element={<TaskPage />} />
